92545f68be908281b37b76d8a537addd067e1843
James Tobin <tobjack330@gmail.com>
2022-06-27 19:39:42 +0100

    cleaned up main.cpp and removed extra compiled .exe files from montecarlo_cpp folder

--- montecarlo_cpp/main.cpp
+++ montecarlo_cpp/main.cpp
@@ -18,38 +18,44 @@
 using namespace std;
 
 int main() {
+  
   // set random seed
   srand(time(NULL));
 
-  // simulation parameters
-  const int n_simulations = 10000;
-  const int n_years = 30;
-  int periodicity = 1;
+  // compile using:
+  // g++ -shared main.cpp MarkovChainMonteCarlo.cpp -o mcmc.dll
 
-  // normal distribtion parameters
-  double ann_mean = 0.10;
-  double ann_std = 0.20;
+  // for testing purposes:::
 
-  // init class MarkovChain and get returns data.
-  MarkovChainMonteCarlo mcmc(ann_mean, ann_std, n_simulations, n_years, periodicity);
+  // // simulation parameters
+  // const int n_simulations = 10000;
+  // const int n_years = 30;
+  // int periodicity = 1;
 
-  // generate returns from mcmc class
-  mcmc.generateReturns();
+  // // normal distribtion parameters
+  // double ann_mean = 0.10;
+  // double ann_std = 0.20;
 
-  // get numbers
-  const int n_periods = mcmc.getNumPeriods();
+  // // init class MarkovChain and get returns data.
+  // MarkovChainMonteCarlo mcmc(ann_mean, ann_std, n_simulations, n_years, periodicity);
 
-  // write some to csv
-  std::ofstream myFile("data.csv");
-  for (int i = 0; i < 100; i++) {
-    for (int j = 0; j < n_periods; j++) {
-      myFile << mcmc.returns[i][j] << "\n";
-    }
-    //myFile << "\n";
-  }
+  // // generate returns from mcmc class
+  // mcmc.generateReturns();
+
+  // // get numbers
+  // const int n_periods = mcmc.getNumPeriods();
+
+  // // write some to csv
+  // std::ofstream myFile("data.csv");
+  // for (int i = 0; i < 100; i++) {
+  //   for (int j = 0; j < n_periods; j++) {
+  //     myFile << mcmc.returns[i][j] << "\n";
+  //   }
+  //   //myFile << "\n";
+  // }
   
-  // Close the file
-  myFile.close();
+  // // Close the file
+  // myFile.close();
   
   return 0;
 };
